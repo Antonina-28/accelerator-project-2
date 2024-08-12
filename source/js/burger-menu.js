72557d3a50a9271754e74732a2d16ed5311cf34f
Antonina Sorokan <atretyakova28@yandex.ru>
2024-08-12 20:20:00 +0300

    Добавляет разметку и стили

---
+++ source/js/burger-menu.js
@@ -0,0 +1,14 @@
+const mainNav = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+export function clickMenu() {
+  navToggle.addEventListener('click', () => {
+    if (mainNav.classList.contains('main-nav--closed')) {
+      mainNav.classList.remove('main-nav--closed');
+      mainNav.classList.add('main-nav--opened');
+    } else {
+      mainNav.classList.add('main-nav--closed');
+      mainNav.classList.remove('main-nav--opened');
+    }
+  });
+}
