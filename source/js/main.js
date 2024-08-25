b811712292df2324d47492fc79d8457996d70bd5
Antonina Sorokan <atretyakova28@yandex.ru>
2024-08-25 21:31:41 +0300

    Добавляет swiper, правит стили для блоков hero, tours и др

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,30 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
 // import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
 
 import { clickMenu } from './burger-menu';
 import { validateForm } from './form-validation';
+import { flippingThroughTheSliderHero } from './hero-swiper';
+import { flippingThroughTheSliderTours } from './tours-swiper';
+import { flippingThroughTheSliderTraining } from './training-swiper';
+import { flippingThroughTheSliderReviews } from './reviews-swiper';
 
 clickMenu();
+flippingThroughTheSliderHero();
+flippingThroughTheSliderTours();
+flippingThroughTheSliderTraining();
+flippingThroughTheSliderReviews();
 validateForm();
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   flippingThroughTheSliderHero();
+// //   playVideo();
+// //   flippingPriceTabs();
+// //   flippingThroughTheSlider();
+// //   initTabs();
+// //   vaildateForm();
+// });
