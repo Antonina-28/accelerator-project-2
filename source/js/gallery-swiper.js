df54baa9626eaf4fd331b1f6fd4189f4742eacfa
Antonina Sorokan <atretyakova28@yandex.ru>
2024-08-31 21:19:03 +0300

    Добавляет правки, PP 30/30

--- source/js/gallery-swiper.js
+++ source/js/gallery-swiper.js
@@ -6,7 +6,6 @@ import 'swiper/css/navigation';
 const options = {
   direction: 'horizontal',
   slidesPerView: 2,
-  spaceBetween: 5,
   loop: true,
   slidesPerGroup: 1,
   navigation: {
@@ -17,17 +16,17 @@ const options = {
   breakpoints: {
     320: {
       slidesPerView: 2,
-      spaceBetween: 6,
+      spaceBetween: 5,
     },
     768: {
       slidesPerView: 3,
-      spaceBetween: 4,
+      spaceBetween: 5,
     },
     1440: {
       loop: false,
       enabled: false,
       slidesPerView: 5,
-      spaceBetween: 5,
+      spaceBetween: 0,
     },
   },
 };
